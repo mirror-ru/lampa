9ad38abda735e58d2a6b7a0bc602bce0ebd5cddb
mirror-ru <mirrorcswm@yandex.ru>
2025-11-16 01:18:48 +0300

    visible_buttons

---
+++ visible_buttons.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict';	
+    Lampa.Listener.follow('full', function (e) {
+        if (e.type == 'complite') {
+            $('.hide.buttons--container > div').prependTo('.full-start-new__buttons');
+            $('.full-start__button.selector.button--play').remove(); 
+        }
+    });
+})();
